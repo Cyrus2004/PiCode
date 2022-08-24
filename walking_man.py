e14667da9ef684ee33dfe664f7f84da1f791148e
Cyrus2004 <cybaa001@osloskolen.no>
2022-08-24 08:48:49 +0200

    a

--- walking_man.py
+++ walking_man.py
@@ -10,18 +10,54 @@ window = turtle.Screen()
 window.title("Shapeshifter")
 window.bgcolor("red")
 
-sprite = turtle.Turtle()
-sprite.shape("triangle")
+
+class Shape(turtle.Turtle):
+    def __init__(self, colors, shapes):
+        turtle.Turtle.__init__(self)
+        self.penup()
+        self.colors = colors
+        self.shapes = shapes
+        self.frame = 0
+
+    def animate(self):
+        self.frame = self.frame + 1
+        if self.frame == len(self.colors):
+            self.frame = 0
+        self.color(self.colors[self.frame])
+        self.shape(self.shapes[self.frame])
+        
+        window.ontimer(self.animate, 500)
 
 
-def sprite_animate():
-    if sprite.shape() == "triangle":
-        sprite.shape("circle")
-    else:
-        sprite.shape("triangle")
-    
-    window.ontimer(sprite_animate, 500)
-sprite_animate()
+shape1Colors = ["black",
+    "green",
+    "blue"
+]
+
+shape1Shapes = [
+    "triangle",
+    "square",
+    "circle"
+]
+
+shape2Colors = ["green",
+    "blue",
+    "green"
+]
+
+shape2Shapes = [
+    "circle",
+    "triangle",
+    "square"
+]
+
+shape1 = Shape(shape1Colors, shape1Shapes)
+shape1.goto(-100, 0)
+shape1.animate()
+
+shape2 = Shape(shape1Colors, shape1Shapes)
+shape2.goto(100, 0)
+shape2.animate()
 
 
 while True:
